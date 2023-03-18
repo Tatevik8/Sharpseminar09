cb713da061635897f50993de9ced29c946952aad
Tatevik8 <tvk25@bk.ru>
2023-03-18 10:14:25 +0300

    Program write all natural numbers between 1 and N.

---
+++ domzad1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 63: Задайте значение N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+int ReadInt(string prompt)
+{
+    Console.WriteLine(prompt);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void WriteNumbers(int num)
+{
+    if (num == 0) return;
+    WriteNumbers(num - 1); 
+    Console.Write(num + " ");
+    }
+
+WriteNumbers(ReadInt("Введите число: "));
+
+
+
+
+// // Выводит числа от большего к меньшему.
+// // N = 5 -> "5, 4, 3, 2, 1"
+
+// int ReadInt(string prompt)
+// {
+//     Console.WriteLine(prompt);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// void WriteNumbers(int num)
+// {
+//     if (num == 0) return;
+//     Console.Write(num + " ");
+//     WriteNumbers(num - 1);
+// }
+
+// WriteNumbers(ReadInt("Введите число: "));
