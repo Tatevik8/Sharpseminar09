ed1453e2ac43952e337bf0059310e58c22176942
Tatevik8 <tvk25@bk.ru>
2023-03-18 13:41:51 +0300

    Program calculates the Ackermann function using recursion.

---
+++ domzad3/Program.cs
@@ -0,0 +1,23 @@
+﻿// **Задача 68:**
+// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int ReadInt(string prompt)
+{
+    Console.Write(prompt);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Ackermann(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m != 0 && n == 0) return Ackermann(m - 1, 1);
+    if (m > 0 && n > 0) return Ackermann(m - 1, Ackermann(m, n - 1));
+    return Ackermann(m, n);
+}
+
+int m = ReadInt("Введите неотрицательное число m: ");
+int n = ReadInt("Введите неотрицательное число n: ");
+Console.WriteLine($"Вычисление функции Аккермана:  {Ackermann(m, n)}");
